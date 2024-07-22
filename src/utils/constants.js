7aa7d0449322db5f7374a24e3e773fe55913f2c2
Le Quoc Huy <wir2404@gmail.com>
2024-07-22 10:55:36 +0700

    Fix domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 export const WHITELIST_DOMAINS = [
   // Không cần local host nữa vì file config cors đã luôn luôn cho phép môi trường dev if (env.BUILD_MODE === 'dev')
   // 'http://localhost:5173'
+  'https://trello-web-roan.vercel.app'
 ];
 
 export const BOARD_TYPES = {
