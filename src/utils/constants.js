8d047e212d8ff4ceb35806b991e62b18e30992e8
Le Quoc Huy <wir2404@gmail.com>
2024-07-22 10:21:45 +0700

    Update CORS not allow localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
+  // Không cần local host nữa vì file config cors đã luôn luôn cho phép môi trường dev if (env.BUILD_MODE === 'dev')
+  // 'http://localhost:5173'
 ];
 
 export const BOARD_TYPES = {
